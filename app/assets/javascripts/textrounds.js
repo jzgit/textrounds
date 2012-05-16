eee2972f08becb51bbb0354a82b56d8989daf008
Jason Zopf <jasonzopf@gmail.com>
2012-05-16 00:30:59 -0400

    Initial project commit

---
+++ app/assets/javascripts/textrounds.js
@@ -0,0 +1,11 @@
+var textrounds = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+
+  init: function(){
+
+  }
+};
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textrounds::Application.config.secret_token = 'dc01f09fb7e54745aad623a323b329c319fad85e6cf730d2fd1653b3fe48c754a4741b82e55a4737d32542bbc8c2d10b673c23e1a9ff6200ba8df1ab94d375ce'
