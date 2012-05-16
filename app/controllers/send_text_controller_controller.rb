5003c5eac790fded9ecabce9b0e888c0605be622
Jason Zopf <jasonzopf@gmail.com>
2012-05-16 04:36:47 -0400

    add jin

--- app/controllers/send_text_controller_controller.rb
+++ app/controllers/send_text_controller_controller.rb
@@ -9,13 +9,20 @@ class SendTextControllerController < ApplicationController
     twilio_token = ENV['TWILIO_TOKEN']
     twilio_phone_number = "(215) 352-5455"
 
+
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
-    @twilio_client.account.sms.messages.create(
-      :from => "+1#{twilio_phone_number}",
-      :to => '2154706093',
-      :body => "Hey Jin this is Curbside - Just wanted to check in"
-    )
+    numbers = ['2154706093','2157407075']
+
+    numbers.each do |num|
+      @twilio_client.account.sms.messages.create(
+        :from => "+1#{twilio_phone_number}",
+        :to => num,
+        :body => "Hey Jin this is Curbside - Just wanted to check in"
+      )
+    end
+
+
     flash[:notice] = 'Message sent'
     redirect_to root_path
   end
